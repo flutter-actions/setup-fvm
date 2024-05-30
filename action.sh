168fd3d614df98ac7ac15c7bec273679cff73836
Socheat Sok <socheatsok78@gmail.com>
2024-05-30 18:32:06 +0700

    Fix test workflow

--- action.sh
+++ action.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export FVM_VERSION=${1:-"latest"}
+export FVM_CACHE_PATH="${RUNNER_TEMP}/fvm_cache"
+export FVM_GIT_CACHE_PATH="${RUNNER_TEMP}/fvm_git_cache"
 exec "${GITHUB_ACTION_PATH}/hacks/install.sh" ${FVM_VERSION}
